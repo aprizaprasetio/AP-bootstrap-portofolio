8499ade169f5bf2681c23346df4126746cda0dcc
Apriza Prasetio <aprizaprasetio@gmail.com>
2022-09-23 17:01:20 +0700

    Fix Responsif & ScrollTop(WIP)

---
+++ assets/js/my-script.js
@@ -0,0 +1,8 @@
+const scrollBtn = document.querySelector('.scroll-top')
+
+// if (window.scrollY > 400) {
+//     scrollBtn.classList.remove('collapse')
+//     return
+// } else {
+//     scrollBtn.classList.add('collapse')
+// }
