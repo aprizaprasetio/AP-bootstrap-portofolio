5900ae1c9363944ef6053fddb27375c2bbd45958
Apriza Prasetio <aprizaprasetio@gmail.com>
2022-09-24 21:45:39 +0700

    ScrollTop & Functional Contact Form

--- assets/js/my-script.js
+++ assets/js/my-script.js
@@ -1,8 +1,34 @@
-const scrollBtn = document.querySelector('.scroll-top')
+// Function Initialization
+const scrollToTop = () => {
+    $('html').animate({
+        scrollTop: 0
+    }, 'slow')
+}
+const collapseScrollBtn = () => {
+    const isDown = $(window).scrollTop() > 250
+    if (isDown) {
+        $('.scroll-top').removeClass('collapse')
+        return
+    }
+    $('.scroll-top').addClass('collapse')
+}
+const sendToSheets = Event => {
+    Event.preventDefault()
+    const myContainer = new FormData(myForm)
+    fetch('https://script.google.com/macros/s/AKfycby4stEtjf-BNNJXr0lPJvX5wiTc-_D44xSEOfuzzrpVG3Vf-Y8_tIaNXi6amehUjesNZA/exec', {
+        method: 'POST',
+        body: myContainer,
+        mode: 'no-cors'
+    })
+        .then(
+            () => {
+                $('#formKontak').modal('hide')
+                $('#infoSukses').modal('show')
+            }
+        )
+}
 
-// if (window.scrollY > 400) {
-//     scrollBtn.classList.remove('collapse')
-//     return
-// } else {
-//     scrollBtn.classList.add('collapse')
-// }
+// Event Initialization ft. JQuery
+$(window).scroll(collapseScrollBtn)
+$('.scroll-top').click(scrollToTop)
+$('#myForm').submit(sendToSheets)
